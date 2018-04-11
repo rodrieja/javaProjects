c8e3aba54e34fb3043b867f2ecdf80f6158183a4
DELL-INSPIRON\rodri <rodriguezalejandro.e@gmail.com>
2018-04-11 08:18:22 -0300

    lectura de archivos

---
+++ archivosDeTexto/src/main/java/Main.java
@@ -0,0 +1,42 @@
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			/*
+			 * Apertura del fichero y creacion de BufferedReader para poder
+			 * hacer una lectura comoda (disponer del metodo readLine()).
+			 */
+			fr = new FileReader("C:\\source\\archivo.txt");
+			br = new BufferedReader(fr);
+
+			// Lectura del fichero
+			String linea;
+			while ((linea = br.readLine()) != null)
+				System.out.println(linea);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			/*
+			 * En el finally cerramos el fichero, para asegurarnos que se cierra
+			 * tanto si todo va bien como si salta una excepcion.
+			 */
+			try {
+				if (fr != null) {
+					fr.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+
+	}
+
+}
