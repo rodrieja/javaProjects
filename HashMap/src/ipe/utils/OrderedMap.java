7b5ed205a098438ad16af7c4284f7dfccaa16afb
DELL-INSPIRON\rodri <rodriguezalejandro.e@gmail.com>
2018-05-02 00:24:45 -0300

    Con javadoc

--- HashMap/src/ipe/utils/OrderedMap.java
+++ HashMap/src/ipe/utils/OrderedMap.java
@@ -4,6 +4,13 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class OrderedMap {
+	/***
+	 * Este método recibe un mapa a ordenar de forma descendente
+	 * @param unsortedMap
+	 *            mapa a ordenar, se utilza Map para hacerlo genérico y no
+	 *            depender del tipo de dato
+	 * @return
+	 */
 	public static Map sortMapByValue(Map unsortedMap) {
 		Map sortedMap = new TreeMap(new HashMapValueComparator(unsortedMap));
 		sortedMap.putAll(unsortedMap);
